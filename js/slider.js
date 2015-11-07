a37160f10b490633ca95c0215d9af0f9e2a18b04
Lindsay Goff <lindsayagoff@gmail.com>
2015-11-06 18:48:58 -0600

    Alternate way to create slideshow using jQuery

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+    $(function() {
+        $('.slideshow img:gt(0)').hide();
+        setInterval(function() {
+            $('.slideshow :first-child')
+                .fadeOut('slow')
+                .next('img')
+                .fadeIn('slow')
+                .end()
+                .appendTo('.slideshow');
+        }, 4000);
+    });
